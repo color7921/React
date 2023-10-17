a370d483d8c4613cd6dfaa0100e3f8efc95cb4d9
color7921 <132988693+color7921@users.noreply.github.com>
2023-10-17 18:01:13 +0900

    React 연습하기
    - 박스 오피스를 검색하는 코드 작성

--- src/04/Box.js
+++ src/04/Box.js
@@ -1,101 +1,60 @@
-import { useState, useEffect } from "react";
-import style from './Box.module.css';
-
+import Hh1 from "../comm/Hh1"
+import styles from './Box.module.css';
+import { useRef, useState, useEffect } from "react";
 const Box = () => {
 
-    const [boxlist, setBoxlist] = useState();
-    const [listTag, setListTag] = useState();
-    const [detailTag,setdetailTag] = useState();
+  
+    async function logJSONData() {
+        const response = await fetch("https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230918");
+        const jsonData = await response.json();
+        console.log(jsonData);
+      }
 
-    const handleClick = (item) => {
-        console.log(item);
-        setdetailTag(
-            <div className={style.numDiv}>
-            <span>영화 코드 : {item.movieCd}  </span>
-            <span>개봉일 : {item.openDt}</span>
-            </div>
-        )
+    //날짜선택
+    const dt = useRef();
+
+    //선택된 날짜
+    const [cdt, setCdt] = useState();
+
+    const handleClick = () => {
+        //cnt1 = cnt1+1;
+        let temp = dt.current.value.replaceAll('-', '');
+        //temp = temp.replaceAll('-', '');
+        // -를 없앰
+        setCdt(temp);
     }
 
-//     useEffect(() => {
-//         let url2 = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=f5eef3421c602c6cb7ea224104795888&movieCd=20124079'
-    
-//     fetch(url2)
-//     .then(resp=> resp.json())
-//     .then(data => setdetailTag(data.movieInfoResult.movieInfo))
-//     .catch(err => console.log(err));
+    //컴포넌트 생성시 포커스 호출
+    useEffect(() => {
+
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        //dt.current.focus();
+        let y = `${yesterday.getFullYear()}`;
+        let m = yesterday.getMonth() + 1 < 10 ? `0${yesterday.getMonth() + 1}`: `${yesterday.getMonth() + 1}`
+        let d = yesterday.getDate() < 10 ? `0${yesterday.getDate()}` : `${yesterday.getDate}`;
         
-// })
+        //어제날짜로 기본값 설정
+        dt.current.value = `${y}-${m}-${d}`;
+        setCdt(y+m+d);
+    }, []);
 
-// useEffect(() => {
-//     if(boxlist){
-//         setdetailTag(boxlist.map((item, idx) =>
-//         <tr key={'mv' + idx} onClick={() => handleClick(item)}>
-//                     <td>{item.movieCd}</td>
-//                     <td>{item.movieNm}</td>
-//                     <td>{item.prdtStatNm}</td>
-//                 </tr>
-//         ))
-//     }
-
-// })
-
-    //컴포넌트 생성시 한 번 실행
-    useEffect(() => {
-        let url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230918'
-        fetch(url)
-            .then(resp => resp.json())
-            .then(data => setBoxlist(data.boxOfficeResult.dailyBoxOfficeList))
-            .catch(err => console.log(err));
-    }, []);//state와는 다르게 함수임 상태가 변할때마다 계속 실행됨.
-
-    //state 변수가 변경될 때마다 실행
-    useEffect(() => {
-        console.log(boxlist);
-        if (boxlist) {
-            setListTag(boxlist.map((item, idx) =>
-                <tr key={'mv' + idx} onClick={() => handleClick(item)}>
-                    <td>{item.rank}</td>
-                    <td>{item.movieNm}</td>
-                    <td>{parseInt(item.salesAmt).toLocaleString('ko-KR')}</td>
-                    <td>{
-                        item.rankInten === '0'
-                            ? "-"
-                            : item.rankInten > 0
-                                ? "▲" + Math.abs(item.rankInten)
-                                : "▼" + Math.abs(item.rankInten)
-                    }
-                    </td>
-                    
-                </tr>
-            ));
-        }
-    }, [boxlist]);
-
+    useEffect(() => {}, [cdt]);
 
     return (
         <main className="container">
+            <Hh1 title='박스오피스' />
+
             <article>
-                <header><h1>일일박스 오피스</h1></header>
-                <table>
-                    <thead>
-                        <tr>
-                            <th scope="col">순위</th>
-                            <th scope="col">영화명</th>
-                            <th scope="col">매출액</th>
-                            <th scope="col">증감</th>
-                           
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {listTag}
-                    </tbody>
-                </table>
-                <footer>
-                    <div>{detailTag}</div>
-                </footer>
+                <header>
+                    <div className={styles.dt}>선택한 날짜 : {cdt} </div>
+                    <form>
+                        <input onChange={handleClick} ref={dt} type='date' id='dt' name='dt' />
+                    </form>
+                </header>
             </article>
         </main>
-    );
+    )
 }
-    export default Box;
+
+export default Box
